ade503d56d40e01848e644ac8fd75d7b7843c406
mtaylorpdx <taylor.matt@protonmail.com>
2020-01-13 21:21:55 -0800

    add text reversal

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,8 @@ var inputText = prompt("Tell me your words:");
 var firstLetter = inputText.charAt(0).toUpperCase();
 var lastLetter = inputText[inputText.length -1].toUpperCase();
 
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 
-alert(lastLetter + " " + firstLetter);
+alert(lastLetter + reverseString(inputText) + firstLetter);
